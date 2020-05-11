04bd324982ee098c55ae314e3d5f401e37058982
rk2609 <accounts@wanderon.in>
2020-05-12 04:53:47 +0530

    Updates

---
+++ precache-manifest.160ab5c907c06b174f8df0bfa3f4352e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9833bff4f48b211541481a948b0a33d8",
+    "url": "/newApp/index.html"
+  },
+  {
+    "revision": "47592eb0ba95bee080d3",
+    "url": "/newApp/static/css/main.ede67ed6.chunk.css"
+  },
+  {
+    "revision": "85ac45151ed06ac93255",
+    "url": "/newApp/static/js/2.dde52f7b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/newApp/static/js/2.dde52f7b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "47592eb0ba95bee080d3",
+    "url": "/newApp/static/js/main.6cacb317.chunk.js"
+  },
+  {
+    "revision": "5fe371bad1cd6d6d888c",
+    "url": "/newApp/static/js/runtime-main.0e638fec.js"
+  }
+]);
